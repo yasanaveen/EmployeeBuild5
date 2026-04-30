67a18e119680b4d174f5ca53fe31154314492abc
Naveen Yasa <yasanaveen535@gmail.com>
2026-04-30 12:45:29 +0530

    new

--- src/test/java/pageObjects/EmployeePageObjects.java
+++ src/test/java/pageObjects/EmployeePageObjects.java
@@ -68,13 +68,13 @@ public class EmployeePageObjects {
     @FindBy(xpath = "(//*[contains(@class,'button__fINWZ')])[1]")
     WebElement managerDropdown;
 
-    @FindBy(xpath = "//*[text()='Ava Davis (HYD000020)']")
+    @FindBy(xpath = "//*[text()='Ravi Kumar (HYD000001)']")
     WebElement managerSelectOption;
 
     @FindBy(xpath = "(//*[contains(@class,'button__fINWZ')])[2]")
     WebElement reportingManagerDropdown;
 
-    @FindBy(xpath = "//*[text()='Ava Davis (HYD000020)']")
+    @FindBy(xpath = "//*[text()='Ravi Kumar (HYD000001)']")
     WebElement reportingManagerSelectOption;
 
     @FindBy(xpath = "//*[@name='remarks']")
